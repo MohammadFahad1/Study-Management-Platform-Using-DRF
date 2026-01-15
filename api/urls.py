da32d19d490b05341aa7d75a1956ef153bff3b67
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-01-15 11:55:49 +0600

    All Matching routes working fine

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework_nested import routers
-from study_materials.views import FlashCardViewSet, FlashCardItemViewSet, QuizViewSet, QuizQuestionViewSet
+from study_materials.views import FlashCardViewSet, FlashCardItemViewSet, QuizViewSet, QuizQuestionViewSet, MatchingViewSet, MatchingItemViewSet
 
 router = routers.DefaultRouter()
 router.register('flashcards', FlashCardViewSet, basename='flashcards')
 router.register('quizzes', QuizViewSet, basename='quizzes')
+router.register('matchings', MatchingViewSet, basename='matchings')
 
 flashcard_router = routers.NestedDefaultRouter(router, 'flashcards', lookup='flashcard')
 flashcard_router.register('items', FlashCardItemViewSet, basename='flashcard-items')
@@ -12,11 +13,15 @@ flashcard_router.register('items', FlashCardItemViewSet, basename='flashcard-ite
 quizzes_router = routers.NestedDefaultRouter(router, 'quizzes', lookup='quiz')
 quizzes_router.register('questions', QuizQuestionViewSet, basename='quiz-questions')
 
+matching_router = routers.NestedDefaultRouter(router, 'matchings', lookup='matching')
+matching_router.register('items', MatchingItemViewSet, basename='matching-items')
+
 
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(flashcard_router.urls)),
     path('', include(quizzes_router.urls)),
+    path('', include(matching_router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt'))
 ]
--- study_materials/views.py
+++ study_materials/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from study_materials.models import FlashCard, FlashCardItem, Quiz, QuizQuestion, Matching, MatchingItem, Note
-from study_materials.serializers import FlashCardSerializer, FlashCardItemSerializer, QuizSerializer, QuizQuestionSerializer
+from study_materials.serializers import FlashCardSerializer, FlashCardItemSerializer, QuizSerializer, QuizQuestionSerializer, MatchingSerializer, MatchingItemSerializer
 from rest_framework.exceptions import PermissionDenied
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -63,3 +63,28 @@ class QuizQuestionViewSet(ModelViewSet):
         if self.request.user != Quiz.objects.get(pk=self.kwargs['quiz_pk']).user:
             raise PermissionDenied("You do not have permission to add questions to this quiz.")
         serializer.save(quiz_id=self.kwargs['quiz_pk'])
+
+
+class MatchingViewSet(ModelViewSet):
+    serializer_class = MatchingSerializer
+
+    def get_queryset(self):
+        return Matching.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class MatchingItemViewSet(ModelViewSet):
+    serializer_class = MatchingItemSerializer
+
+    def get_queryset(self):
+        if 'matching_pk' in self.kwargs:
+            matching = Matching.objects.get(pk=self.kwargs['matching_pk'])
+            if matching.user != self.request.user:
+                raise PermissionDenied("You do not have permission to view items for this matching.")
+        return MatchingItem.objects.filter(matching=self.kwargs['matching_pk'])
+    
+    def perform_create(self, serializer):
+        if self.request.user != Matching.objects.get(pk=self.kwargs['matching_pk']).user:
+            raise PermissionDenied("You do not have permission to add items to this matching.")
+        serializer.save(matching_id=self.kwargs['matching_pk'])
